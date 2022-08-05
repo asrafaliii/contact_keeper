c38caad2260247e59118ddcc1268aa2022da4647
Asraf Ali <asraf.bookaand@gmail.com>
2022-08-05 11:05:27 +0600

    import data from Mockaroo

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { data } from "../components/Data";
 
 const TableData = () => {
+  //   const [constacts, setContacts] = useState(data);
+  const [search, setSearch] = useState("");
+
+  // const sortName = () => {
+  //   setContacts(
+  //     data.sort((a, b) => {
+  //       return a.first_name.toLowerCase() < a.first_name.toLowerCase()
+  //         ? -1
+  //         : a.first_name.toLowerCase() > a.first_name.toLowerCase()
+  //         ? 1
+  //         : 0;
+  //     })
+  //   );
+  // };
+
   return (
     <div>
       <Container>
         <h1 className="text-center mt-4">Contact Keeper</h1>
         <Form>
           <InputGroup className="my-3">
-            <Form.Control placeholder="Search contacts" />
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search contacts"
+            />
           </InputGroup>
         </Form>
         <Table striped bordered hover>
@@ -24,12 +43,20 @@ const TableData = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            {data
+              .filter((item) => {
+                return search.toLowerCase() === ""
+                  ? item
+                  : item.first_name.toLowerCase().includes(search);
+              })
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              ))}
           </tbody>
         </Table>
       </Container>
